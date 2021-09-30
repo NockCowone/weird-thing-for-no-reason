94ff775a530ceaa839a0cf66817ca8bbbf0ea633
NockCowone <69426192+NockCowone@users.noreply.github.com>
2021-09-30 10:12:22 -0400

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"      ///   AAAA   HHH  HHH\n     ///   AAAAAA  HHH  HHH\n    ///   AA    AA HHHHHHHH\n   ///    AAAAAAAA HHH  HHH\n  ///     AA    AA HHH  HHH\n ///      AA    AA HHH  HHH\n///       AA    AA HHH  HHH";
+cout<<"\nTTTTTTT    III  MMM    MMM   EEEEEEE  DDDDDD  EEEEEEE OOOOOOO\n  TTT      III  MMMM  MMMM   EEE      DDD  DD EEE     OOO OOO\n  TTT      III  MMM MM MMM   EEEEEEE  DDD  DD EEEEEEE OOO OOO\n  TTT      III  MMM MM MMM   EEE      DDD  DD EEE     OOO OOO\n  TTT      III  MMM    MMM   EEEEEEE  DDDDDD  EEEEEEE OOOOOOO";
+    return 0;
+}
